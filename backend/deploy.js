08d121ec0f3427a3cfd055c14bc7fd9989728ab5
Leejun <93887188+lkim0402@users.noreply.github.com>
2025-04-26 16:49:14 +0900

    few updates

--- backend/deploy.js
+++ backend/deploy.js
@@ -1,87 +1,3 @@
-// import {
-//   //   paginateListBuckets,
-//   S3Client,
-//   S3ServiceException,
-//   PutObjectCommand,
-// } from "@aws-sdk/client-s3";
-// import fs from "fs";
-// import path from "path";
-// import process from "process";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const mimeTypes = {
-//   ".html": "text/html",
-//   ".css": "text/css",
-//   ".js": "application/javascript",
-//   ".json": "application/json",
-//   ".png": "image/png",
-//   ".jpg": "image/jpeg",
-//   ".jpeg": "image/jpeg",
-//   ".svg": "image/svg+xml",
-//   ".txt": "text/plain",
-// };
-
-// const BUCKET_NAME = "cloud-deploy-bucket-01";
-
-// export const uploadFile = async (filePath, keyName) => {
-//   const client = new S3Client({
-//     region: "us-east-1",
-//     credentials: {
-//       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     },
-//   });
-
-//   try {
-//     const fileContent = fs.readFileSync(filePath);
-
-//     // Dynamically set the file's MIME type based on file extension (e.g., text/html for .txt)
-//     const ext = path.extname(filePath).toLowerCase();
-//     const contentType = mimeTypes[ext] || "application/octet-stream"; // Add more MIME types if needed
-
-//     const uploadParams = {
-//       Bucket: BUCKET_NAME,
-//       Key: keyName,
-//       Body: fileContent,
-//       ContentType: contentType, // change based on file type
-//       ACL: "public-read", // makes file publicly accessible
-//     };
-
-//     const command = new PutObjectCommand(uploadParams);
-//     const response = await client.send(command);
-//     console.log("File uploaded successfully: ", response);
-
-//     return generateS3Link;
-//   } catch (err) {
-//     if (err instanceof S3ServiceException) {
-//       console.error(`${err.name}: ${err.message}`);
-//     } else {
-//       throw err;
-//     }
-//   }
-// };
-
-// // returning the bucket link
-// const generateS3Link = (BUCKET_NAME, keyName) => {
-//   return `https://${BUCKET_NAME}.s3.amazonaws.com/${keyName}`;
-// };
-
-// export const uploadDir = async (dirPath, basePath = dirPath) => {
-//   const entries = fs.readdirSync(dirPath);
-
-//   for (const entry of entries) {
-//     const entryPath = path.join(dirPath, entry);
-
-//     if (fs.lstatSync(entryPath).isDirectory()) {
-//       await uploadDir(entryPath, basePath); // recurse
-//     } else {
-//       const relativePath = path.relative(basePath, entryPath);
-//       await uploadFile(entryPath, relativePath);
-//     }
-//   }
-// };
-
 import {
   S3Client,
   PutObjectCommand,
@@ -91,7 +7,6 @@ import fs from "fs";
 import path from "path";
 import process from "process";
 import dotenv from "dotenv";
-import generate from "@babel/generator";
 dotenv.config();
 
 const mimeTypes = {
@@ -118,9 +33,9 @@ const client = new S3Client({
 
 export const uploadAndGetS3Link = async (dirPath) => {
   try {
-    await uploadDir(dirPath);
+    const fileNum = await uploadDir(dirPath);
     const s3Link = generateS3Link();
-    return s3Link;
+    return { filenum: fileNum, link: s3Link };
   } catch (e) {
     console.error("Error uploading directory: ", e);
     throw e;
@@ -159,28 +74,29 @@ const uploadFile = async (filePath, keyName) => {
 
 // Recursive function to upload all files in the directory
 export const uploadDir = async (dirPath, basePath = dirPath) => {
-  const entries = fs.readdirSync(dirPath);
+  const entries = fs.readdirSync(dirPath); // array
   let fileCount = 0;
-  let uploadedFiles = [];
 
   for (const entry of entries) {
     const entryPath = path.join(dirPath, entry);
 
+    // if directory
     if (fs.lstatSync(entryPath).isDirectory()) {
-      const { uploadedFiles: nestedFiles, fileCount: nestedCount } =
-        await uploadDir(entryPath, basePath);
-      uploadedFiles = [...uploadedFiles, ...nestedFiles];
+      const { fileCount: nestedCount } = await uploadDir(entryPath, basePath);
+
+      // updating values
       fileCount += nestedCount;
-    } else {
+    }
+    // if file
+    else {
       const relativePath = path.relative(basePath, entryPath);
       await uploadFile(entryPath, relativePath);
-      uploadedFiles.push(generateS3Link(BUCKET_NAME, relativePath));
       fileCount++;
     }
   }
 
   // Once upload is complete, return the list of uploaded files' links
-  return { uploadedFiles, fileCount };
+  return fileCount;
 };
 
 export const generateS3Link = () => {
